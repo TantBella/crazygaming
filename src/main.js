b832659330ead052973919ed046811ac078043eb
CodeByJohan <johan.gotet@gmail.com>
2023-02-20 14:41:38 +0100

    Added BootstrapVueNext

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 import App from './App.vue'
 import router from './router'
