898c203cd2ce9f5d3da3d6163c2432f2238fc782
emi-river <emi.riverdi@gmail.com>
2023-02-21 16:06:08 +0100

    Add header desktop version

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -13,10 +13,12 @@
     <img src="/assets/Logo.png" alt="Crazy Gaming logo." />
   </div>
   <Search />
-  <!-- <RouterLink to=""> -->
-  <main>
-    <RouterView />
-  </main>
+  <div id="category">
+    <RouterLink to="/products/Xbox">Xbox</RouterLink>
+    <RouterLink to="/products/Playstation">Playstation</RouterLink>
+    <RouterLink to="/products/Nintendo">Nintendo</RouterLink>
+    <RouterLink to="/products/PC">PC</RouterLink>
+  </div>
 </template>
 
 <style lang="scss">
@@ -25,6 +27,15 @@
     justify-content: center;
     margin: 1em 0;
   }
+  #category {
+    visibility: hidden;
+  }
   @media (min-width: 600px) {
+    #category {
+      visibility: visible;
+      display: flex;
+      justify-content: space-evenly;
+      margin-bottom: 1em;
+    }
   }
 </style>
