acf12bc5c5278d99c1f382be3bb52efbc281e300
cwacode <williamandersson27@gmail.com>
2023-02-23 10:44:09 +0100

    Finished NewsCarousel and changed location to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,14 @@
-<template><h1>Hem</h1></template>
+<script>
+  import NewsCarousel from '../components/NewsCarousel.vue'
+
+  export default {
+    components: {
+      NewsCarousel
+    }
+  }
+</script>
+
+<template>
+  <NewsCarousel />
+  <h1>Hem</h1>
+</template>
