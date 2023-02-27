98b84726584325ac5e6ea4cae3c8091c43316cc7
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-02-27 11:50:45 +0100

    Fixed homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,22 @@
-<!-- <script>
+<script>
   import NewsCarousel from '../products/NewsCarousel.vue'
-  import ProductPage from '../products/Productview.vue'
 
   export default {
     components: {
-      NewsCarousel,
-      ProductPage
+      NewsCarousel
     }
   }
 </script>
 
 <template>
   <NewsCarousel />
-  <ProductPage />
-  <div class="prodcontainer">
-    <h1>Products and Categories</h1>
-    <ul v-for="product in productsID" :key="product">
-      <li>
-        {{ product.name }}
-      </li>
-    </ul>
+  <div>
+    <h1>bajs</h1>
   </div>
 </template>
 
-<style lang="scss" scoped>
+<!-- <style lang="scss" scoped>
   .prodcontainer {
     background-color: rgba(240, 248, 255, 0.111);
   }
 </style> -->
-
-<script></script>
-<template>
-  <h1>hej</h1>
-</template>
