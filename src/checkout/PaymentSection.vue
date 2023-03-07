92a9206e6bf8367e41fec417bc95f50617cdd39c
cwacode <williamandersson27@gmail.com>
2023-03-07 15:33:47 +0100

    made changes in CheckoutView + components

--- src/checkout/PaymentSection.vue
+++ src/checkout/PaymentSection.vue
@@ -1,16 +1,75 @@
 <template>
-  <div>
-    <button @click="nextSection('cart')">Back</button>
+  <div id="payment-section">
+    <h2>Payment Information</h2>
+    <form @submit.prevent="submitPayment">
+      <label for="card-number">Card Number:</label>
+      <input type="text" id="card-number" v-model="cardNumber" required />
+
+      <label for="expiration-date">Expiration Date:</label>
+      <input
+        type="text"
+        id="expiration-date"
+        v-model="expirationDate"
+        required
+      />
+
+      <label for="security-code">Security Code:</label>
+      <input type="text" id="security-code" v-model="securityCode" required />
+
+      <button type="submit">Submit Payment</button>
+    </form>
+
+    <div v-if="paymentSuccess">
+      Payment successful! Thank you for your order.
+    </div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
-    emits: ['next-section'],
+    data() {
+      return {
+        cardNumber: '',
+        expirationDate: '',
+        securityCode: '',
+        paymentSuccess: false
+      }
+    },
     methods: {
-      nextSection(section) {
-        this.$emit('next-section', section)
+      submitPayment() {
+        // Send payment information to server
+        axios
+          .post('/api/payment', {
+            cardNumber: this.cardNumber,
+            expirationDate: this.expirationDate,
+            securityCode: this.securityCode
+          })
+          .then(() => {
+            // Payment successful
+            this.paymentSuccess = true
+            // Update order status or navigate to confirmation page
+          })
+          .catch((error) => {
+            // Payment failed
+            console.error(error)
+            // Show error message to user
+          })
       }
     }
   }
 </script>
+
+<style scoped>
+  #payment-section {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 2rem;
+    background-color: #3a0ca370;
+    border-radius: 25px;
+    margin-top: 1.5rem;
+    margin-bottom: 1.5rem;
+    color: #fff;
+  }
+</style>
--- src/checkout/ShippingSection.vue
+++ src/checkout/ShippingSection.vue
@@ -1,16 +1,73 @@
 <template>
-  <div>
-    <button @click="nextSection('payment')">Next</button>
+  <div id="shipping-section">
+    <h2>Shipping Information</h2>
+    <form @submit.prevent="submitShipping">
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="name" required />
+
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email" required />
+
+      <label for="phone">Phone Number:</label>
+      <input type="tel" id="phone" v-model="phone" required />
+
+      <label for="address">Shipping Address:</label>
+      <textarea id="address" v-model="address" required />
+
+      <label>Shipping Options:</label>
+      <ul>
+        <li v-for="option in shippingOptions" :key="option.id">
+          <input
+            type="radio"
+            :id="option.id"
+            :value="option"
+            v-model="selectedOption"
+          />
+          <label :for="option.id">{{ option.name }} - {{ option.price }}</label>
+        </li>
+      </ul>
+
+      <button type="submit">Next</button>
+    </form>
   </div>
 </template>
 
 <script>
   export default {
-    emits: ['next-section'],
+    data() {
+      return {
+        name: '',
+        email: '',
+        phone: '',
+        address: '',
+        shippingOptions: [
+          { id: 'option1', name: 'Standard', price: '$5.00' },
+          { id: 'option2', name: 'Express', price: '$10.00' }
+        ],
+        selectedOption: null
+      }
+    },
     methods: {
-      nextSection(section) {
-        this.$emit('next-section', section)
+      submitShipping() {
+        // Validate shipping information
+        // ...
+
+        // Notify parent component that shipping information is complete
+        this.$emit('next-section', 'payment')
       }
     }
   }
 </script>
+
+<style scoped>
+  #shipping-section {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 2rem;
+    background-color: #3a0ca370;
+    border-radius: 25px;
+    margin-top: 1.5rem;
+    margin-bottom: 1.5rem;
+    color: #fff;
+  }
+</style>
