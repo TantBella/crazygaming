14e288c4e775eff52159c61705232ac4aaa6285b
ivajunu <ivanna.junuzovic@live.se>
2023-03-11 14:59:11 +0100

    Vuex in SignUp and mypagesview

--- src/views/MyPagesView.vue
+++ src/views/MyPagesView.vue
@@ -2,7 +2,23 @@
 
 <template>
   <div class="mypages">
-    <h1>Welcome Testsson!</h1>
+    <div class="welcome">
+      <h1>
+        Welcome {{ this.$store.state.registeredUser.firstname }}
+        {{ this.$store.state.registeredUser.lastname }}! Ready to game?
+      </h1>
+      <p>
+        Here you can find personalised links to your account, your wishlist,
+        your purchases, track your order and many other things! See this as
+        <u>your</u>
+        personalised space.
+      </p>
+      <p>
+        You can log in to your account using the emailadress that you used when
+        you registered the account.
+      </p>
+    </div>
+    <!-- Kanske hade varit bra att ha "mitt konto" typ också som en länk? -->
     <div class="userbtn">
       <RouterLink to="/favorites"> My Wishlist </RouterLink>
       <img
@@ -61,6 +77,10 @@
 </template>
 
 <style lang="scss" scoped>
+  .welcome {
+    margin: 10px;
+    text-align: center;
+  }
   .mypages {
     background-color: #a180f698;
     padding: 5px 0 20px 0;
